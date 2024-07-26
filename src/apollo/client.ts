41b0c1f8f63e121e3d0b10cb7917132f08459c84
0xALUCARD <0xALUCARD@protonmail.com>
2024-07-26 20:57:41 +0200

    remove eth subgraphs and update to 1.2.0 subgraph

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -6,7 +6,7 @@ export const healthClient = new ApolloClient({
 })
 
 export const blockClient = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+  uri: '',
   cache: new InMemoryCache(),
   queryDeduplication: true,
   defaultOptions: {
@@ -21,7 +21,7 @@ export const blockClient = new ApolloClient({
 })
 
 export const client = new ApolloClient({
-  uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3?source=uniswap',
+  uri: '',
   cache: new InMemoryCache({
     typePolicies: {
       Token: {
@@ -49,7 +49,7 @@ export const client = new ApolloClient({
 })
 
 export const fantomClient = new ApolloClient({
-  uri: 'https://api.goldsky.com/api/public/project_clss7z2o15cxh010fdvq909x6/subgraphs/fantom-v3/1.0.0/gn',
+  uri: 'https://api.goldsky.com/api/public/project_clss7z2o15cxh010fdvq909x6/subgraphs/fantom-v3/1.2.0/gn',
   cache: new InMemoryCache({
     typePolicies: {
       Token: {
