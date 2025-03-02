910522e6907fd90703504fa02bc7ecf27deb5590
0xALUCARD <0xALUCARD@protonmail.com>
2025-03-02 01:45:37 +0100

    update

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,29 +1,13 @@
 // used to mark unsupported tokens, these are hosted lists of unsupported tokens
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
-export const OPTIMISM_LIST = ''
-export const ARBITRUM_LIST = ''
-export const POLYGON_LIST = ''
-export const CELO_LIST = ''
-export const BNB_LIST = ''
-export const FANTOM_LIST = 'https://assets.spooky.fi/ftm_spooky_tokens.json'
-export const EON_LIST = 'https://assets.spooky.fi/eon_spooky_tokens.json'
-export const BTTC_LIST = 'https://assets.spooky.fi/btt_spooky_tokens.json'
 export const GOAT_TESTNET_LIST = 'https://assets.goatswap.fi/goat_testnet_goat_tokens.json'
 export const GOAT_LIST = 'https://sxpppvinxvbb6utl.public.blob.vercel-storage.com/goatswap/goat_tokens.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  BTTC_LIST,
-  EON_LIST,
-  FANTOM_LIST,
-  GOAT_TESTNET_LIST,
+  // GOAT_TESTNET_LIST,
   GOAT_LIST,
-  OPTIMISM_LIST,
-  ARBITRUM_LIST,
-  POLYGON_LIST,
-  CELO_LIST,
-  BNB_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
