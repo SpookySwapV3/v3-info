c9696d0c2c94ecf6989ac78935d608dd927f75ab
0xALUCARD <0xALUCARD@protonmail.com>
2025-03-13 21:46:46 +0100

    github token list

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -2,7 +2,7 @@
 
 export const UNSUPPORTED_LIST_URLS: string[] = []
 export const GOAT_TESTNET_LIST = 'https://assets.goatswap.fi/goat_testnet_goat_tokens.json'
-export const GOAT_LIST = 'https://sxpppvinxvbb6utl.public.blob.vercel-storage.com/goatswap/goat_tokens.json'
+export const GOAT_LIST = 'https://raw.githubusercontent.com/SpookySwapV3/token-lists/refs/heads/main/Goat/goat_tokens.json'
 
 // lower index == higher priority for token import
 export const DEFAULT_LIST_OF_LISTS: string[] = [
